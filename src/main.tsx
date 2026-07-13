bdc2d68ce18358711bace5da4bd6341130850035
Fuhao574 <Fuhao574@126.com>
2026-07-13 11:27:06 +0800

    将字体文件打包至本地

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+// 本地化字体（无需外网请求）
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/noto-sans-sc/300.css'
+import '@fontsource/noto-sans-sc/400.css'
+import '@fontsource/noto-sans-sc/500.css'
+import '@fontsource/noto-sans-sc/600.css'
+import '@fontsource/noto-sans-sc/700.css'
+
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
